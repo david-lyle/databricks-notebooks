a88eeab5bb6b6cbe48c3ca421faf5d16d0cc21db
david.lyle@databricks.com <david.lyle@databricks.com>
2022-03-03 22:27:25 +0000

    Various changes for customer

--- Autoload Usage Information.py
+++ Autoload Usage Information.py
@@ -1,39 +1,22 @@
 # Databricks notebook source
-# MAGIC %md
-# MAGIC # Cost Accounting on Databricks GCP <img src="/files/images/320px_Databricks_Logo.png" width=160 height=160 />
-# MAGIC 
-# MAGIC #### 1. Enforcing Tags with Cluster Policies
-# MAGIC Users (Belong to a Group) -> Groups (Associated with a Cost Center) -> Cluster Policy (Enforce Cost Center Tagging) -> Clusters (Tagged with Correct Cost Center) <P>
-# MAGIC [Manage Cluster Policies](https://docs.gcp.databricks.com/administration-guide/clusters/policies.html)
-# MAGIC #### 2. Usage Reporting
-# MAGIC [View billable usage using the account console](https://docs.gcp.databricks.com/administration-guide/account-settings-gcp/usage.html)
-# MAGIC #### 3. Downloading CSV - Currently Manual (Accounts API in Private Preview)
-# MAGIC [AWS Example of Download via Account API](https://docs.databricks.com/administration-guide/account-settings/billable-usage-download-api.html)
-# MAGIC #### 4. Parsing CSV Using Databricks
-# MAGIC [Analyze billable usage log data](https://docs.databricks.com/administration-guide/account-settings/usage-analysis.html)
+dbutils.widgets.text('Database', '')
+dbutils.widgets.text('Checkpoint Path', '')
+dbutils.widgets.text('Schema Location', '')
+dbutils.widgets.text('Autoload File Path', '')
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC use  dlyle
+spark.sql("use " + getArgument('Database'))
 
 # COMMAND ----------
 
-dbutils.fs.rm(checkpoint_path,recurse=True)
-dbutils.fs.rm(write_path, recurse=True)
-
 spark.sql("""
-drop table if exists billingusage
-""")
-
-spark.sql("""
-create table billingusage(workspaceId string, timestamp timestamp, 
+create table if not exists billingusage(workspaceId string, timestamp timestamp, 
                           clusterId string, clusterName string, 
                           clusterNodeType string, clusterOwnerUserId string, 
                           sku string, dbus double, machineHours double, 
                           clusterOwnerUserName string, tags map<string, string>, cost double)
 """)
-
 
 # COMMAND ----------
 
@@ -59,10 +42,9 @@ from pyspark.sql.functions import *
 from pyspark.sql.window import Window
 import pyspark.sql.functions as func
 
-usagefilePath = "dbfs:/FileStore/shared_uploads/david.lyle@databricks.com/itemizedusage"
-checkpoint_path = 'dbfs:/dlyle/itemizedusage/_checkpoints'
-write_path = 'dbfs:/dlyle/itemizedusage/delta/usage_data'
-schema_location = 'dbfs:/dlyle/itemizedusage/schema'
+usagefilePath = getArgument('Autoload File Path')
+checkpoint_path = getArgument('Checkpoint Path')
+schema_location = getArgument('Schema Location')
 
 
 df = (spark.readStream.format("cloudFiles") 
@@ -119,70 +101,3 @@ streamingQuery = (usageDF.writeStream
   .foreachBatch(drop_duplicates)  
   .start()                  
    )          
-
-
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select count(*) from billingusage
-
-# COMMAND ----------
-
-# Build usage schema
-usageSchema = StructType([
-  StructField("workspaceId", StringType(), False),
-  StructField("timestamp", TimestampType(), False),
-  StructField("clusterId", StringType(), False),
-  StructField("clusterName", StringType(), False),
-  StructField("clusterNodeType", StringType(), False),
-  StructField("clusterOwnerUserId", StringType(), False),
-  StructField("clusterCustomTags", StringType(), False),
-  StructField("sku", StringType(), False),
-  StructField("dbus", FloatType(), False),
-  StructField("machineHours", FloatType(), False),
-  StructField("clusterOwnerUserName", StringType(), False),
-  StructField("tags", StringType(), False)
-])
-
-#Read file to dataframe - dbfs:/FileStore/2022_01_2022_01.csv
-usagefilePath = "dbfs:/FileStore/shared_uploads/david.lyle@databricks.com/itemizedusage/2021_04_2022_03.csv"
-
-# Instantiate and cache the usage dataframe
-df = (spark.read
-      .option("header", "true")
-      .option("escape", "\"")
-      .schema(usageSchema)
-      .csv(usagefilePath)
-      )
-df.createOrReplaceTempView("usage")
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select count(*), workspaceId, timestamp, clusterId from usage group by workspaceId, timestamp, clusterId order by count(1) desc
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select * from usage where workspaceId = '1559018344558186' and timestamp = '2021-12-21T14:59:59.000+0000' and clusterId = '1221-120530-dingy494'
-
-# COMMAND ----------
-
-# MAGIC %sql 
-# MAGIC select clusterName, sku, sum(dbus) as dubs, round(sum(cost),2) as cost from usage where tags.CostCenter == 10011 group by clusterName, sku
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select coalesce(tags.CostCenter,"NONE") as costCenter, sku, sum(dbus) as dbus, round(sum(cost),2) as cost_in_dollars from usage group by costCenter, sku order by cost_in_dollars desc
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select distinct(clusterName) as clusterName, clusterOwnerUsername from usage where tags.CostCenter is null
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select clusterName, sku, round(sum(cost),2) as cost_in_dollars from usage group by clusterName, sku order by cost_in_dollars desc
--- legacy_notebooks/Streaming Driver for Testing.py
+++ legacy_notebooks/Streaming Driver for Testing.py
@@ -1,4 +1,28 @@
 # Databricks notebook source
+# MAGIC %sql
+# MAGIC 
+# MAGIC --CREATE database dlyle;
+# MAGIC create or replace table dlyle.queryprogress (
+# MAGIC batchId bigint,
+# MAGIC durationMs struct<addBatch:bigint,getBatch:bigint,latestOffset:bigint,queryPlanning:bigint,triggerExecution:bigint,walCommit:bigint>,
+# MAGIC id string,
+# MAGIC inputRowsPerSecond double,
+# MAGIC name string,
+# MAGIC numInputRows bigint,
+# MAGIC processedRowsPerSecond double,
+# MAGIC runId string,
+# MAGIC sink struct<description:string,numOutputRows:bigint>,
+# MAGIC sources array<struct<description:string,endOffset:bigint,inputRowsPerSecond:double,latestOffset:bigint,numInputRows:bigint,processedRowsPerSecond:double,startOffset:bigint>>,
+# MAGIC stateOperators array<string>,
+# MAGIC timestamp string,
+# MAGIC progressDate DATE GENERATED ALWAYS AS (TO_DATE(timestamp))
+# MAGIC )
+# MAGIC using delta
+# MAGIC partitioned by (progressDate)
+# MAGIC TBLPROPERTIES (delta.autoOptimize.optimizeWrite = true, delta.autoOptimize.autoCompact = true)
+
+# COMMAND ----------
+
 # MAGIC %scala 
 # MAGIC import org.apache.http.client.methods.HttpPut
 # MAGIC import org.apache.http.impl.client.HttpClientBuilder
@@ -21,15 +45,12 @@
 # MAGIC 
 # MAGIC class PushGatewayWipingListener(pushGatewayUrl: String) extends StreamingQueryListener {
 # MAGIC   
-# MAGIC     // Modify StreamingQueryListener Methods 
 # MAGIC     override def onQueryStarted(event: QueryStartedEvent): Unit = {
 # MAGIC       System.out.println("Query started: " + event.name); }
 # MAGIC     override def onQueryTerminated(event: QueryTerminatedEvent ): Unit = {
 # MAGIC       wipePushgateway("http://35.223.225.182")
 # MAGIC       System.out.println("Query terminated: " + event.id); }
-# MAGIC    override def onQueryProgress(event: QueryProgressEvent): Unit = {
-# MAGIC       //System.out.println("Query made progress: " + event.progress);
-# MAGIC     }
+# MAGIC    override def onQueryProgress(event: QueryProgressEvent): Unit = {}
 # MAGIC   }
 # MAGIC 
 # MAGIC val pgWipingListener = new PushGatewayWipingListener("http://35.223.225.182")
@@ -37,7 +58,52 @@
 # COMMAND ----------
 
 # MAGIC %scala
+# MAGIC import org.apache.spark.sql.streaming._
+# MAGIC import StreamingQueryListener._
+# MAGIC import scala.collection.mutable.ListBuffer
+# MAGIC import org.apache.spark.internal.Logging
+# MAGIC 
+# MAGIC def writeToDelta(seq : ListBuffer[String], tableName: String): Unit = {    
+# MAGIC     var json_ds = seq.toDS()
+# MAGIC     val df = spark.read.json(json_ds)
+# MAGIC     df.write.format("delta").mode("append").saveAsTable(tableName)    
+# MAGIC }
+# MAGIC 
+# MAGIC class QueryProgressWritingListener() extends StreamingQueryListener with Logging {
+# MAGIC   var json_seq = new ListBuffer[String]()
+# MAGIC     override def onQueryStarted(event: QueryStartedEvent): Unit = {}
+# MAGIC     override def onQueryTerminated(event: QueryTerminatedEvent ): Unit = {
+# MAGIC       if( !json_seq.isEmpty ){
+# MAGIC         writeToDelta(json_seq, "dlyle.queryprogress")
+# MAGIC       }
+# MAGIC     }
+# MAGIC     override def onQueryProgress(event: QueryProgressEvent): Unit = {  
+# MAGIC       json_seq += event.progress.json   
+# MAGIC       if( json_seq.length == 120)
+# MAGIC       try{
+# MAGIC           writeToDelta(json_seq, "dlyle.queryprogress")                
+# MAGIC           json_seq.clear() 
+# MAGIC       }catch {
+# MAGIC         case t: Throwable => {          
+# MAGIC           logError("Error writing QPO to Delta", t)
+# MAGIC           throw t
+# MAGIC         }
+# MAGIC       }      
+# MAGIC     }
+# MAGIC   }
+# MAGIC 
+# MAGIC val qpWritingListener = new QueryProgressWritingListener()
+
+# COMMAND ----------
+
+# MAGIC %scala
 # MAGIC spark.streams.addListener(pgWipingListener)
+# MAGIC spark.streams.addListener(qpWritingListener)
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC spark.streams.listListeners
 
 # COMMAND ----------
 
@@ -52,15 +118,20 @@ rateDf = spark \
 
 # COMMAND ----------
 
+def foreach_batch_function(df, epoch_id):
+    df.collect()
+
+# COMMAND ----------
+
 
 spark.conf.set("spark.sql.shuffle.partitions", "8")  # keep the size of shuffles small
-
+ 
 query = (
   rateDf
     .writeStream
-    .format("memory")        # memory = store in-memory table 
-    .queryName("rates")     # rates = name of the in-memory table
-    .outputMode("append") 
+    .queryName("WriteToMemory")
+    .trigger(processingTime = "1 second")
+    .foreachBatch(foreach_batch_function)                
     .start()
 )
 
@@ -68,5 +139,5 @@ query = (
 
 # MAGIC %python
 # MAGIC from time import sleep
-# MAGIC sleep(60) 
-# MAGIC query.stop()
+# MAGIC #sleep(300) 
+# MAGIC #query.stop()
